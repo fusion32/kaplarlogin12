657f44e430b05d07e831edd2d2819924db63831d
fusion32 <marcopuzziello@gmail.com>
2022-06-25 23:13:03 -0300

    we're now loading and sending info from the database
    - The only features left to implement are the event schedule and
      HTTPS support.

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
 	"database/sql"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -10,16 +13,36 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type RequestError struct {
-	ErrorCode    int64  `json:"errorCode"`
-	ErrorMessage string `json:"errorMessage"`
-}
+// TODO(fusion): Perhaps have these values loaded from a config file?
+const (
+	cfgWorldName     = "Canary"
+	cfgGameHost      = "localhost"
+	cfgGamePort      = 7172
+	cfgWorldLocation = "BRA" // this must be (?) "USA", "EUR", or "BRA"
+	cfgWorldType     = "pvp" // this must be "pvp", "no-pvp", or "pvp-enforced"
+	cfgDBUser        = "root"
+	cfgDBPwd         = "senha123"
+	cfgDBHost        = "localhost:3306"
+	cfgDBName        = "canary"
+	cfgDBUseTLS      = false
+)
+
+var (
+	// NOTE(fusion): Handle to the connected database. This should be the only
+	// global variable.
+	dbHandle *sql.DB
+)
 
 type ClientRequest struct {
 	RequestType  string `json:"type"`
 	Email        string `json:"email,omitempty"`
 	Password     string `json:"password,omitempty"`
 	StayLoggedIn bool   `json:"stayloggedin,omitempty"`
+}
+
+type RequestError struct {
+	ErrorCode    int64  `json:"errorCode"`
+	ErrorMessage string `json:"errorMessage"`
 }
 
 type BoostedCreature struct {
@@ -79,8 +102,8 @@ type WorldInfo struct {
 	ExternalPortProtected      int64  `json:"externalportprotected"`
 	ExternalAddressUnprotected string `json:"externaladdressunprotected"`
 	ExternalPortUnprotected    int64  `json:"externalportunprotected"`
-	Location                   string `json:"location"` // USA, EUR, BRA
-	WorldType                  string `json:"pvptype"`  // pvp, no-pvp, pvp-enforced
+	Location                   string `json:"location"`
+	WorldType                  string `json:"pvptype"`
 	AntiCheatProtection        bool   `json:"anticheatprotection"`
 	RestrictedStore            bool   `json:"restrictedstore"`
 	Unused1                    bool   `json:"istournamentworld"`
@@ -145,8 +168,8 @@ func SendCacheInfo(w http.ResponseWriter, playersOnline int64) {
 	w.Write(encoded)
 }
 
-func SendEventSchedule(w http.ResponseWriter, eventList []EventInfo, lastUpdate time.Time) {
-	eventSchedule := EventSchedule{eventList, lastUpdate.Unix()}
+func SendEventSchedule(w http.ResponseWriter, eventList []EventInfo, lastUpdate int64) {
+	eventSchedule := EventSchedule{eventList, lastUpdate}
 	encoded, err := json.Marshal(eventSchedule)
 	if err != nil {
 		panic("failed to encode EventSchedule struct")
@@ -169,6 +192,276 @@ func SendLoginResponse(w http.ResponseWriter, session *SessionInfo, worlds []Wor
 	w.Write(encoded)
 }
 
+func HandleBoostedCreatureRequest(w http.ResponseWriter) {
+	// TODO(fusion): Do we want to check `boosted_creature`.`date`?
+	enabled := true
+	raceId := int64(35)
+	row := dbHandle.QueryRow("SELECT `raceid` FROM `boosted_creature`")
+	if err := row.Scan(&raceId); err != nil {
+		fmt.Printf("failed to load boosted creature: %v\n", err)
+		enabled = false
+	}
+	SendBoostedCreature(w, enabled, raceId)
+}
+
+func HandleCacheInfoRequest(w http.ResponseWriter) {
+	playersOnline := int64(0)
+	row := dbHandle.QueryRow("SELECT COUNT(*) FROM `players_online`")
+	if err := row.Scan(&playersOnline); err != nil {
+		fmt.Printf("failed to count online players: %v\n", err)
+	}
+	SendCacheInfo(w, playersOnline)
+}
+
+func HandleEventScheduleRequest(w http.ResponseWriter) {
+	var eventList []EventInfo
+
+	// NOTE(fusion): It seems that the client doesn't display information about
+	// the exact time it starts or ends.
+
+	const oneDay = 24 * time.Hour
+	today := time.Now().Truncate(oneDay)
+
+	{ // Test Event 1
+		startDate := today
+		endDate := today.Add(oneDay)
+		tmp := EventInfo{
+			Name:            "Test Event 1",
+			StartDate:       startDate.Unix(),
+			EndDate:         endDate.Unix(),
+			SpecialEvent:    0,
+			DisplayPriority: 1,
+			IsSeasonal:      false,
+			Description:     "Test Event 1 Description",
+			ColorLight:      "#2D7400",
+			ColorDark:       "#235C00",
+		}
+		eventList = append(eventList, tmp)
+	}
+
+	{ // Test Event 2
+		startDate := today.Add(2 * oneDay)
+		endDate := today.Add(4 * oneDay)
+		tmp := EventInfo{
+			Name:            "Test Event 2",
+			StartDate:       startDate.Unix(),
+			EndDate:         endDate.Unix(),
+			SpecialEvent:    0,
+			DisplayPriority: 0,
+			IsSeasonal:      false,
+			Description:     "Test Event 2 Description",
+			ColorLight:      "#2D74FF",
+			ColorDark:       "#235CFF",
+		}
+		eventList = append(eventList, tmp)
+	}
+
+	{ // Test Event 3
+		startDate := today.Add(3 * oneDay)
+		endDate := today.Add(5 * oneDay)
+		tmp := EventInfo{
+			Name:            "Test Event 3",
+			StartDate:       startDate.Unix(),
+			EndDate:         endDate.Unix(),
+			SpecialEvent:    0,
+			DisplayPriority: 0,
+			IsSeasonal:      true,
+			Description:     "Test Event 3 Description",
+			ColorLight:      "#2D74FF",
+			ColorDark:       "#235CFF",
+		}
+		eventList = append(eventList, tmp)
+	}
+
+	{ // Test Event 4
+		startDate := today.Add(-15 * oneDay)
+		endDate := today.Add(oneDay)
+		tmp := EventInfo{
+			Name:            "Test Event 4",
+			StartDate:       startDate.Unix(),
+			EndDate:         endDate.Unix(),
+			SpecialEvent:    0,
+			DisplayPriority: 0,
+			IsSeasonal:      false,
+			Description:     "Test Event 4 Description",
+			ColorLight:      "#FF7423",
+			ColorDark:       "#FF5C23",
+		}
+		eventList = append(eventList, tmp)
+	}
+
+	SendEventSchedule(w, eventList, time.Now().Unix())
+}
+
+func TestPassword(pwd string, hash string) bool {
+	// TODO(fusion): Add support for different hashing schemes.
+	pwdHash := sha1.Sum([]byte(pwd))
+	testHash, err := hex.DecodeString(hash)
+	if err != nil {
+		return false
+	}
+
+	return bytes.Compare(pwdHash[:], testHash) == 0
+}
+
+func GetVocationName(id int64) string {
+	switch id {
+	default:
+	case 0:
+		return "None"
+
+	case 1:
+		return "Sorcerer"
+	case 2:
+		return "Druid"
+	case 3:
+		return "Paladin"
+	case 4:
+		return "Knight"
+
+	case 5:
+		return "Master Sorcerer"
+	case 6:
+		return "Elder Druid"
+	case 7:
+		return "Royal Paladin"
+	case 8:
+		return "Elite Knight"
+	}
+
+	// NOTE(fusion): Without this we get a "missing return" error.
+	return "None"
+}
+
+func GetDailyRewardState(pendingReward bool) int64 {
+	if pendingReward {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func HandleLoginRequest(w http.ResponseWriter, r *ClientRequest) {
+	var (
+		err         error
+		accId       int64
+		accPwdHash  string
+		accPremDays int64
+	)
+
+	accRow := dbHandle.QueryRow(
+		"SELECT `id`, `password`, `premdays`"+
+			" FROM `accounts` WHERE `email` = ?", r.Email)
+	err = accRow.Scan(&accId, &accPwdHash, &accPremDays)
+	if err != nil || !TestPassword(r.Password, accPwdHash) {
+		SendRequestError(w, 3, "Invalid email or password.")
+		return
+	}
+
+	accPremEnd := int64(0)
+	if accPremDays > 0 {
+		const oneDay = 24 * time.Hour
+		tmp := time.Now()
+		tmp.Truncate(oneDay)
+		tmp.Add(time.Duration(accPremDays) * oneDay)
+		accPremEnd = tmp.Unix()
+	}
+
+	chRows, err := dbHandle.Query(
+		"SELECT `name`, `level`, `sex`, `vocation`, `looktype`,"+
+			" `lookhead`, `lookbody`, `looklegs`, `lookfeet`, `lookaddons`,"+
+			" `lastlogin`, `isreward`, `istutorial`"+
+			" FROM `players` WHERE `account_id` = ?", accId)
+	if err != nil {
+		SendRequestError(w, 1, "Internal error.")
+		return
+	}
+	defer chRows.Close()
+
+	accLastLogin := int64(0)
+	characters := make([]CharacterInfo, 0)
+	for chRows.Next() {
+		var (
+			chName          string
+			chLevel         int64
+			chSex           int64
+			chVocId         int64
+			chLookType      int64
+			chLookHead      int64
+			chLookBody      int64
+			chLookLegs      int64
+			chLookFeet      int64
+			chLookAddons    int64
+			chLastLogin     int64
+			chPendingReward bool
+			chIsTutorial    bool
+		)
+
+		err = chRows.Scan(&chName, &chLevel, &chSex, &chVocId, &chLookType,
+			&chLookHead, &chLookBody, &chLookLegs, &chLookFeet, &chLookAddons,
+			&chLastLogin, &chPendingReward, &chIsTutorial)
+		if err != nil {
+			SendRequestError(w, 1, "Internal error.")
+			return
+		}
+
+		// NOTE(fusion): Keep the most recent "lastlogin".
+		if chLastLogin > accLastLogin {
+			accLastLogin = chLastLogin
+		}
+
+		characters = append(characters, CharacterInfo{
+			WorldId:          0,
+			Name:             chName,
+			Level:            chLevel,
+			Vocation:         GetVocationName(chVocId),
+			LookType:         chLookType,
+			LookHead:         chLookHead,
+			LookBody:         chLookBody,
+			LookLegs:         chLookLegs,
+			LookFeet:         chLookFeet,
+			LookAddons:       chLookAddons,
+			DailyRewardState: GetDailyRewardState(chPendingReward),
+			IsMale:           chSex == 1,
+			Tutorial:         chIsTutorial,
+			IsHidden:         false,
+			IsMainCharacter:  false,
+		})
+	}
+
+	// TODO(fusion): This session key looks weird enough. Does the client
+	// expect this or does it expect an UID?
+	session := SessionInfo{
+		SessionKey:            r.Email + "\n" + r.Password,
+		Status:                "active", // TODO(fusion)
+		LastLogin:             accLastLogin,
+		PremiumEnd:            accPremEnd,
+		IsPremium:             accPremEnd > 0,
+		ReturningPlayer:       false,
+		ReturningNotification: false,
+		ShowRewardNews:        false,
+		FpsTracking:           false,
+		OptionTracking:        false,
+	}
+
+	world := WorldInfo{
+		Id:                         0,
+		Name:                       cfgWorldName,
+		ExternalAddress:            cfgGameHost,
+		ExternalPort:               cfgGamePort,
+		ExternalAddressProtected:   cfgGameHost,
+		ExternalPortProtected:      cfgGamePort,
+		ExternalAddressUnprotected: cfgGameHost,
+		ExternalPortUnprotected:    cfgGamePort,
+		Location:                   cfgWorldLocation,
+		WorldType:                  cfgWorldType,
+		AntiCheatProtection:        false,
+		RestrictedStore:            false,
+	}
+
+	SendLoginResponse(w, &session, []WorldInfo{world}, characters)
+}
+
 func RequestHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/login.php" || r.Method != http.MethodPost {
 		return
@@ -183,152 +476,24 @@ func RequestHandler(w http.ResponseWriter, r *http.Request) {
 	var clRequest ClientRequest
 	if err := json.Unmarshal(data, &clRequest); err != nil {
 		fmt.Printf("failed to decode ClientRequest struct: %v", err)
-		SendRequestError(w, 3, "Ill-formed request.")
+		SendRequestError(w, 1, "Ill-formed request.")
 		return
 	}
 
 	fmt.Printf("%T: %+v\n", clRequest, clRequest)
-
 	switch clRequest.RequestType {
 	case "boostedcreature":
-		SendBoostedCreature(w, true, 2)
+		HandleBoostedCreatureRequest(w)
 	case "cacheinfo":
-		SendCacheInfo(w, 123)
+		HandleCacheInfoRequest(w)
 	case "eventschedule":
-		var eventList []EventInfo
-
-		// NOTE(fusion): It seems that the client doesn't display information about
-		// the exact time it starts or ends.
-
-		{ // Test Event 1
-			startDate := time.Date(2022, 6, 25, 12, 15, 0, 0, time.Local)
-			endDate := time.Date(2022, 6, 26, 13, 30, 0, 0, time.Local)
-			tmp := EventInfo{
-				Name:            "Test Event 1",
-				StartDate:       startDate.Unix(),
-				EndDate:         endDate.Unix(),
-				SpecialEvent:    0,
-				DisplayPriority: 1,
-				IsSeasonal:      false,
-				Description:     "Test Event 1 Description",
-				ColorLight:      "#2D7400",
-				ColorDark:       "#235C00",
-			}
-			eventList = append(eventList, tmp)
-		}
-
-		{ // Test Event 2
-			startDate := time.Date(2022, 6, 27, 6, 0, 0, 0, time.Local)
-			endDate := time.Date(2022, 6, 30, 8, 0, 0, 0, time.Local)
-			tmp := EventInfo{
-				Name:            "Test Event 2",
-				StartDate:       startDate.Unix(),
-				EndDate:         endDate.Unix(),
-				SpecialEvent:    0,
-				DisplayPriority: 0,
-				IsSeasonal:      false,
-				Description:     "Test Event 2 Description",
-				ColorLight:      "#2D74FF",
-				ColorDark:       "#235CFF",
-			}
-			eventList = append(eventList, tmp)
-		}
-
-		{ // Test Event 3
-			startDate := time.Date(2022, 6, 29, 6, 0, 0, 0, time.Local)
-			endDate := time.Date(2022, 8, 1, 6, 0, 0, 0, time.Local)
-			tmp := EventInfo{
-				Name:            "Test Event 3",
-				StartDate:       startDate.Unix(),
-				EndDate:         endDate.Unix(),
-				SpecialEvent:    0,
-				DisplayPriority: 0,
-				IsSeasonal:      true,
-				Description:     "Test Event 3 Description",
-				ColorLight:      "#2D74FF",
-				ColorDark:       "#235CFF",
-			}
-			eventList = append(eventList, tmp)
-		}
-
-		{ // Test Event 4
-			startDate := time.Date(2022, 6, 10, 0, 0, 0, 0, time.Local)
-			endDate := time.Date(2022, 6, 26, 0, 0, 0, 0, time.Local)
-			tmp := EventInfo{
-				Name:            "Test Event 4",
-				StartDate:       startDate.Unix(),
-				EndDate:         endDate.Unix(),
-				SpecialEvent:    0,
-				DisplayPriority: 0,
-				IsSeasonal:      false,
-				Description:     "Test Event 4 Description",
-				ColorLight:      "#FF7423",
-				ColorDark:       "#FF5C23",
-			}
-			eventList = append(eventList, tmp)
-		}
-
-		SendEventSchedule(w, eventList, time.Now())
+		HandleEventScheduleRequest(w)
 	case "login":
-		tmpSession := SessionInfo{
-			SessionKey:            clRequest.Email + "\n" + clRequest.Password,
-			Status:                "active",
-			LastLogin:             0,
-			PremiumEnd:            0,
-			IsPremium:             false,
-			ReturningPlayer:       false,
-			ReturningNotification: false,
-			ShowRewardNews:        false,
-			FpsTracking:           false,
-			OptionTracking:        false,
-		}
-
-		// TODO(fusion): Does this need to be a numeric address?
-		commonAddress := "127.0.0.1"
-		commonPort := int64(7172)
-
-		tmpWorld := WorldInfo{
-			Id:                         0,
-			Name:                       "Canary",
-			ExternalAddress:            commonAddress,
-			ExternalPort:               commonPort,
-			ExternalAddressProtected:   commonAddress,
-			ExternalPortProtected:      commonPort,
-			ExternalAddressUnprotected: commonAddress,
-			ExternalPortUnprotected:    commonPort,
-			Location:                   "BRA",
-			WorldType:                  "pvp",
-			AntiCheatProtection:        false,
-			RestrictedStore:            false,
-		}
-
-		tmpCharacter := CharacterInfo{
-			WorldId:          0,
-			Name:             "Sorcerer Sample",
-			Level:            100,
-			Vocation:         "Sorcerer",
-			LookType:         35,
-			LookHead:         0,
-			LookBody:         0,
-			LookLegs:         0,
-			LookFeet:         0,
-			LookAddons:       0,
-			DailyRewardState: 0,
-			IsMale:           true,
-			Tutorial:         false,
-			IsHidden:         false,
-			IsMainCharacter:  true,
-		}
-
-		SendLoginResponse(w, &tmpSession, []WorldInfo{tmpWorld}, []CharacterInfo{tmpCharacter})
+		HandleLoginRequest(w, &clRequest)
 	default:
-		//SendRequestError(w, 3, "Invalid request type.")
-		return
+		SendRequestError(w, 1, "Invalid request.")
 	}
 }
-
-// NOTE(fusion): Handle to the connected database. This should be the only global variable.
-var dbHandle *sql.DB
 
 func main() {
 	var (
@@ -336,27 +501,19 @@ func main() {
 		dbVersion string
 	)
 
-	// TODO(fusion): Have a config file? Have more parameters?
-	dbUser := "root"
-	dbPwd := "senha123"
-	dbHost := "localhost:3306"
-	dbName := "canary"
-	dbUseTLS := false
+	dataSourceName := fmt.Sprintf("%v:%v@tcp(%v)/%v?&tls=%t",
+		cfgDBUser, cfgDBPwd, cfgDBHost, cfgDBName, cfgDBUseTLS)
 
-	dbDSN := fmt.Sprintf("%v:%v@tcp(%v)/%v?&tls=%t",
-		dbUser, dbPwd, dbHost, dbName, dbUseTLS)
-
-	// NOTE(fusion): We can call dbHandle.Ping instead of querying the
-	// version if we are not interested in it to make sure we are connected
-	// to the database.
-	dbHandle, _ = sql.Open("mysql", dbDSN)
+	// NOTE(fusion): We can call dbHandle.Ping instead of querying the version if
+	// we are not interested in it to make sure we are connected to the database.
+	dbHandle, _ = sql.Open("mysql", dataSourceName)
 	err = dbHandle.QueryRow("SELECT VERSION()").Scan(&dbVersion)
 	if err != nil {
 		fmt.Printf("Failed to connect to the database: %v\n", err)
 		return
 	}
 
-	fmt.Printf("Connected to database \"%v\" on \"%v\"\n", dbName, dbHost)
+	fmt.Printf("Connected to database \"%v\" on \"%v\"\n", cfgDBName, cfgDBHost)
 	fmt.Printf("Database version: %v\n", dbVersion)
 	fmt.Printf("Kaplar login server running...\n")
 
